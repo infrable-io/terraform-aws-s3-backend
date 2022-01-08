abbebe4e050817705c3d6e806e0d8a2af770643d
NickolasHKraus <NickHKraus@gmail.com>
2022-01-08 15:18:19 -0600

    Update outputs.tf

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -3,10 +3,14 @@
 # -----------------------------------------------------------------------------
 output "s3_bucket" {
   value       = module.s3_backend.s3_bucket
-  description = "The name of the S3 bucket"
+  description = <<-EOF
+  The name of the S3 bucket for storing Terraform state
+  EOF
 }
 
 output "dynamodb_table" {
   value       = module.s3_backend.dynamodb_table
-  description = "The name of the DynamoDB table"
+  description = <<-EOF
+  The name of the DynamoDB table for storing Terraform state lock
+  EOF
 }
